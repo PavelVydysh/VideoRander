6ac2a0aa27a2620901a7e81ee216e53e76db78fe
PavelVydysh <vydysh.pavel@gmail.com>
2021-05-25 20:06:59 +0300

    Открытие видеофайла и разьиение на кадры

---
+++ VideoWorker/Form1.Designer.cs
@@ -0,0 +1,115 @@
+﻿
+namespace VideoWorker
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.player = new AxWMPLib.AxWindowsMediaPlayer();
+            this.setVideo = new System.Windows.Forms.OpenFileDialog();
+            this.openFile = new System.Windows.Forms.Button();
+            this.setRect = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            ((System.ComponentModel.ISupportInitialize)(this.player)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // player
+            // 
+            this.player.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.player.Enabled = true;
+            this.player.Location = new System.Drawing.Point(3, 44);
+            this.player.Name = "player";
+            this.player.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("player.OcxState")));
+            this.player.Size = new System.Drawing.Size(787, 493);
+            this.player.TabIndex = 0;
+            // 
+            // setVideo
+            // 
+            this.setVideo.FileName = "openFileDialog1";
+            // 
+            // openFile
+            // 
+            this.openFile.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.openFile.Location = new System.Drawing.Point(3, 4);
+            this.openFile.Name = "openFile";
+            this.openFile.Size = new System.Drawing.Size(85, 34);
+            this.openFile.TabIndex = 1;
+            this.openFile.Text = "Файл";
+            this.openFile.UseVisualStyleBackColor = true;
+            this.openFile.Click += new System.EventHandler(this.openFile_Click);
+            // 
+            // setRect
+            // 
+            this.setRect.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.setRect.Image = global::VideoWorker.Properties.Resources.vD0JqLnzFEs;
+            this.setRect.Location = new System.Drawing.Point(94, 4);
+            this.setRect.Name = "setRect";
+            this.setRect.Size = new System.Drawing.Size(85, 34);
+            this.setRect.TabIndex = 2;
+            this.setRect.UseVisualStyleBackColor = true;
+            this.setRect.Click += new System.EventHandler(this.setRect_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(285, 559);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(316, 56);
+            this.listBox1.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(899, 637);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.setRect);
+            this.Controls.Add(this.openFile);
+            this.Controls.Add(this.player);
+            this.MinimumSize = new System.Drawing.Size(816, 489);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.player)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private AxWMPLib.AxWindowsMediaPlayer player;
+        private System.Windows.Forms.OpenFileDialog setVideo;
+        private System.Windows.Forms.Button openFile;
+        private System.Windows.Forms.Button setRect;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+    }
+}
+
---
+++ VideoWorker/Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing;
+using Emgu;
+using Emgu.CV;
+using Emgu.CV.Structure;
+using Emgu.CV.Util;
+using System.IO;
+
+namespace VideoWorker
+{
+    public partial class Form1 : Form
+    {
+        private static CascadeClassifier classifier = new CascadeClassifier("haarcascade_frontalface_alt_tree.xml");
+        private static VideoCapture vc = null;
+        private static Mat mat = new Mat();
+        private static Image<Bgr, byte> iplImage = null;
+        private static string path = "";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //public Image<Bgr, byte> = null;
+
+        private void openFile_Click(object sender, EventArgs e)
+        {
+            setVideo.Filter = "Video files (*.mp4, *.wm, *.asf, *.wmv, *.avi, *.wtv, *.mpeg, *.mpg, *.mpe, *.m1v, *.mp2, *.mpv2, *.vob) | *.mp4; *.wm; *.asf; *.wmv; *.avi; *.wtv; *.mpeg; *.mpg; *.mpe; *.m1v; *.mp2; *.mpv2; *.vob";
+            if (setVideo.ShowDialog() == DialogResult.OK)
+            {
+                vc = new VideoCapture(setVideo.FileName);
+                path = setVideo.FileName;
+                player.URL = path;
+                player.Ctlcontrols.stop();
+            }
+        }
+
+        private void setRect_Click(object sender, EventArgs e)
+        {
+            int i = 1;
+            try
+            {
+                using (var video = new VideoCapture(path))
+                using (var img = new Mat())
+                {
+                    while (video.Grab())
+                    {
+                        video.Retrieve(img);
+                        var filename = Path.Combine(@"D:\fgh", $"{i}.png");
+                        CvInvoke.Imwrite(filename, img);
+                        i++;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                listBox1.Items.Add(ex.Message);
+            }
+        }
+    }
+}
